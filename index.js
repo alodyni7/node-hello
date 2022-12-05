66e184c610c680746593b35b2a936b1849625a50
alodyni7 <35926328+alodyni7@users.noreply.github.com>
2022-12-06 01:57:00 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const http = require('http');
 const port = process.env.PORT || 3000;
-
+var express = require('express'),
+    app = express(),
+    server = require('http').createServer(app);
+const miniget = require('miniget');
+/*
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   const msg = 'Hello Node!\n'
   res.end(msg);
 });
-
+*/
 server.listen(port, () => {
   console.log(`Server running on http://localhost:${port}/`);
 });
+app.get("/",function (req, res){
+            res.send({err:true});
+})
